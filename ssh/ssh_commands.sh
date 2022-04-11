e173fd078c4043f1c62a17a8ac77ccd3b7453545
Lucas Kivi <lukekivi@gmail.com>
2022-04-10 23:25:12 -0500

    Added SSH script

---
+++ ssh/ssh_commands.sh
@@ -0,0 +1,11 @@
+ssh -f $DFS_USERNAME@csel-kh1250-11.cselabs.umn.edu "cd $PROJ_PATH; ant server"
+ssh -f $DFS_USERNAME@csel-kh1250-12.cselabs.umn.edu "cd $PROJ_PATH; ant server"
+ssh -f $DFS_USERNAME@csel-kh1250-13.cselabs.umn.edu "cd $PROJ_PATH; ant server"
+ssh -f $DFS_USERNAME@csel-kh1250-14.cselabs.umn.edu "cd $PROJ_PATH; ant server"
+ssh -f $DFS_USERNAME@csel-kh1250-15.cselabs.umn.edu "cd $PROJ_PATH; ant server"
+ssh -f $DFS_USERNAME@csel-kh1250-16.cselabs.umn.edu "cd $PROJ_PATH; ant server"
+ssh -f $DFS_USERNAME@csel-kh1250-17.cselabs.umn.edu "cd $PROJ_PATH; ant server"
+ssh -f $DFS_USERNAME@csel-kh1250-18.cselabs.umn.edu "cd $PROJ_PATH; ant clientOne"
+ssh -f $DFS_USERNAME@csel-kh1250-19.cselabs.umn.edu "cd $PROJ_PATH; ant clientTwo"
+ssh -f $DFS_USERNAME@csel-kh1250-20.cselabs.umn.edu "cd $PROJ_PATH; ant clientThree"
+
