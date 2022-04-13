3f86cd95054c464e053c4153d92a691ff8d7330e
Charles Droege <droegecharlie@gmail.com>
2022-04-13 11:38:36 -0500

    Now going to have user change ssh file instead of commands file

--- app/test/test9/ssh_commands.sh
+++ app/test/test9/ssh_commands.sh
@@ -12,8 +12,8 @@ ssh -f $DFS_USERNAME@csel-kh1250-16.cselabs.umn.edu "cd $PROJ_PATH; ant server"
 sleep 2
 ssh -f $DFS_USERNAME@csel-kh1250-17.cselabs.umn.edu "cd $PROJ_PATH; ant server"
 sleep 2
-ssh -f $DFS_USERNAME@csel-kh1250-18.cselabs.umn.edu "cd $PROJ_PATH; ant clientOne"
-ssh -f $DFS_USERNAME@csel-kh1250-19.cselabs.umn.edu "cd $PROJ_PATH; ant clientOne"
-ssh -f $DFS_USERNAME@csel-kh1250-20.cselabs.umn.edu "cd $PROJ_PATH; ant clientOne"
+ssh -f $DFS_USERNAME@csel-kh1250-18.cselabs.umn.edu "cd $PROJ_PATH; ant clientTwo"
+ssh -f $DFS_USERNAME@csel-kh1250-19.cselabs.umn.edu "cd $PROJ_PATH; ant clientTwo"
+ssh -f $DFS_USERNAME@csel-kh1250-20.cselabs.umn.edu "cd $PROJ_PATH; ant clientTwo"
 sleep 10
 ssh -f $DFS_USERNAME@csel-kh1250-21.cselabs.umn.edu "cd $PROJ_PATH; ant clientCheck"
